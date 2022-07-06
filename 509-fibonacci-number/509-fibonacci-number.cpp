053d37e32265a16f314b6cb52c73db715d01c09a
RITIK JAIN <93763090+RJ025@users.noreply.github.com>
2022-07-06 07:47:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,6 +3,15 @@ public:
     int fib(int n) {
         if(n==0 || n==1)return n;
         
-        return fib(n-1) + fib(n-2);
+        int prev1=0 , prev2=1  , curr;
+        
+        for(int i=2 ; i<=n ; i++)
+        {
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        
+        return curr;
     }
 };
