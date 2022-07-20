1560e71db5015e79913abaa1d298b19ef26d12b6
RITIK JAIN <93763090+RJ025@users.noreply.github.com>
2022-07-20 17:09:31 +0530

    Time: 62 ms (62.64%), Space: 22 MB (13.88%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // int ans=0;
+    int code(vector<int>freq)
+    {
+        int ans=0;
+        for(int i=0 ; i<26 ; i++)
+        {
+            if(freq[i]>0)
+            {
+                freq[i]--;
+               ans+=code(freq)+1;
+                freq[i]++;
+            }
+        }
+        
+        return ans;
+    }
+    
+    int numTilePossibilities(string tiles) {
+        if(tiles.size()==0)return 0;
+        
+        vector<int>freq(26 , 0);
+        
+        for(int i=0 ; i<tiles.length() ; i++)
+        {
+            char ch = tiles[i];
+            int idx = ch-'A';
+             freq[idx]++;
+        }
+        // int ans=0;
+        
+        return code(freq);
+        
+        // return ans;
+    }
+};
