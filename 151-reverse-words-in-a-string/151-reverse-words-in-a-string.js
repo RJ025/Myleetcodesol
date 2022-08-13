625fe2fd2b0ad1baf1b5f634983b40e518cb5f70
RITIK JAIN <93763090+RJ025@users.noreply.github.com>
2022-08-13 16:07:24 +0530

    Time: 102 ms (41.35%), Space: 44 MB (60.53%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -9,6 +9,6 @@ var reverseWords = function(s) {
     let arr1 = arr.filter((data)=>{
         return (data.length>0)?true: false;
     })
-    console.log(arr1);
+    // console.log(arr1);
     return arr1.join(' ');
 };
