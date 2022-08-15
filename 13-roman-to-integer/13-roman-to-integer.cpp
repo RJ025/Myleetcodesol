fea218c1ec7b138c23cfdf71dc0278cb38bc88e5
RITIK JAIN <93763090+RJ025@users.noreply.github.com>
2022-08-15 09:21:17 +0530

    Time: 19 ms (50.83%), Space: 8.1 MB (37.92%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        if(n==0)return 0;
+        
+        unordered_map<char , int>mpp;
+        mpp['I'] = 1;
+        mpp['V'] = 5;
+        mpp['X'] = 10;
+        mpp['L'] = 50;
+        mpp['C'] = 100;
+        mpp['D'] = 500;
+        mpp['M'] = 1000;
+        
+        int ans =0;
+        int i=0;
+        
+        while(i<n)
+        {
+            if(s[i]=='I' and s[i+1]=='V'){
+                ans=ans+(mpp['V']-mpp['I']);
+                i+=2;
+            }
+            else if(s[i]=='I' and s[i+1]=='X'){
+                ans=ans+(mpp['X']-mpp['I']);
+                i+=2;
+            }
+            else if(s[i]=='X' and s[i+1]=='L'){
+                ans=ans+(mpp['L']-mpp['X']);
+                i+=2;
+            }
+            else if(s[i]=='X' and s[i+1]=='C'){
+                ans=ans+(mpp['C']-mpp['X']);
+                i+=2;
+            }
+            else if(s[i]=='C' and s[i+1]=='D'){
+                ans=ans+(mpp['D']-mpp['C']);
+                i+=2;
+            }
+            else if(s[i]=='C' and s[i+1]=='M'){
+                ans=ans+(mpp['M']-mpp['C']);
+                i+=2;
+            }
+            else{
+                ans+=mpp[s[i]];
+                i++;
+            }      
+
+        }
+        
+        // cout<<ans;
+        return ans;
+
+    }
+};
