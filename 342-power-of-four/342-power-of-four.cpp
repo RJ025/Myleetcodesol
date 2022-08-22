94d3056cbb401b67eca95c24d152843f81057067
RITIK JAIN <93763090+RJ025@users.noreply.github.com>
2022-08-22 11:10:34 +0530

    Time: 4 ms (40.16%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -5,6 +5,10 @@ public:
         if(n==0)return false;
         if(n==1)return true;
         
-        return (isPowerOfFour(n/4) and n%4==0);
+        if(n%4==0)return isPowerOfFour(n/4);
+        
+        return false;
+        
+        // return ( and n%4==0);
     }
 };
